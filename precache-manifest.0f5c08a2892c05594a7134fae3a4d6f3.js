37c42d1bf62504c7d309591d4d12d45861d0f83b
Simon <semen.zelenskiy@gmail.com>
2019-10-31 15:49:22 +0200

    Updates

---
+++ precache-manifest.0f5c08a2892c05594a7134fae3a4d6f3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b6b59a0cc538ee126de90296ebe9a4",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "aa19a42a60f1b42b0a5d",
+    "url": "/react_list-of-goods/static/js/2.26a51a8b.chunk.js"
+  },
+  {
+    "revision": "13e7c73d683ba0819bf2",
+    "url": "/react_list-of-goods/static/js/main.4eaff97a.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
